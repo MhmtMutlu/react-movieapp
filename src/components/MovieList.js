2fc9ac79c0ef37ded9f585ef7a55b1238499a594
MhmtMutlu <49007453+MhmetMutlu@users.noreply.github.com>
2021-06-06 18:36:22 +0300

    Components created

---
+++ src/components/MovieList.js
@@ -0,0 +1,26 @@
+import React from "react"
+
+const MovieList = (props) => {
+        return (
+            <div className="row">
+                {props.movies.map((movie) => (
+                    <div className="col-lg-4">
+                        <div key={movie.id} className="card mb-4 shadow-lg border border-dark">
+                            <img src={movie.imageURL} className="card-img-top" alt="Sample Movie" />
+                            <div className="card-body">
+                                <h5 className="card-title">{movie.name}</h5>
+                                <p className="card-text">{movie.overview}</p>
+                                <div className="d-flex justify-content-between align-items-center">
+                                    <button type="button" className="btn btn-md btn-outline-danger">Delete</button>
+                                    <h2><span className="badge bg-info">{movie.rating}</span></h2>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                ))}
+                
+            </div>
+        )
+}
+
+export default MovieList
