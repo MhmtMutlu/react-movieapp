697503f5cb3f7d89b9221cd6cc107ff793bf60a4
MhmtMutlu <49007453+MhmetMutlu@users.noreply.github.com>
2021-06-07 13:57:43 +0300

    axios added and implemented

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react"
 import SearchBar from "./components/SearchBar"
 import MovieList from "./components/MovieList"
+import axios from "axios"
 
 
 class App extends React.Component {
@@ -11,14 +12,42 @@ class App extends React.Component {
     }
 
     //! Getting data from src/api/movies.json (fake.api)
+    //! using fetch
+    // async componentDidMount() {
+    //   const baseURL = "http://localhost:3002/movies"
+    //   const response = await fetch(baseURL)
+    //   const data = await response.json()
+    //   this.setState({movies: data})
+    // }
+
+    //! using axios
     async componentDidMount() {
-      const baseURL = "http://localhost:3002/movies"
-      const response = await fetch(baseURL)
-      const data = await response.json()
-      this.setState({movies: data})
+      const response = await axios.get("http://localhost:3002/movies")
+      this.setState({movies: response.data})
     }
 
-    deleteMovie = (movie) => {
+    //! Delete func with fetchapi
+    // deleteMovie = async (movie) => {
+    //   const baseURL = `http://localhost:3002/movies/${movie.id}`
+    //   await fetch(baseURL, {
+    //     method: "DELETE"
+    //   })
+    //   const newMovieList = this.state.movies.filter(
+    //     m => m.id !== movie.id
+    //   )
+    //   //! If we dont have any state value, we can use that codes
+    //   // this.setState({
+    //   //   movies: newMovieList
+    //   // })
+    //   //! If we have state values, we can use that codes
+    //   this.setState(state => ({
+    //     movies: newMovieList
+    //   }))
+    // }
+
+    //! Delete func with axios
+    deleteMovie = async (movie) => {
+      axios.delete(`http://localhost:3002/movies/${movie.id}`)
       const newMovieList = this.state.movies.filter(
         m => m.id !== movie.id
       )
